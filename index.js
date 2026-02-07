9c9b9af9c13cb9b9a9f4386da28f9349736c362f
Kenenn Lara <kenennlara@gmail.com>
2026-02-06 21:10:46 -0600

    Fix counter problem
    Corrected issue with counter, it was adding twice the points since function was being called more than once.

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
   const max = 10;
   const min = 1;
@@ -26,27 +23,37 @@ function getHumanChoice() {
 
 const humanChoice = getHumanChoice();
 const computerChoice = getComputerChoice();
+let humanScore = 0;
+let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
+    humanScore++;
+    computerScore++;
     return "Its a tie!";
   } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore++;
     return "Rock beats scissors! Human wins!";
   } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    computerScore++;
     return "Rock beats scissors! Computer wins!";
   } else if (humanChoice === "paper" && computerChoice === "rock") {
+    humanScore++;
     return "Paper beats rock! Human wins!";
   } else if (humanChoice === "rock" && computerChoice === "paper") {
+    computerScore++;
     return "Paper beats rock!  Computer wins!";
   } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    humanScore++;
     return "Scissors beats paper!  Human wins!";
   } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    computerScore++;
     return "Scissors beats paper!  Computer wins!";
   }
 }
 
-playRound(humanChoice, computerChoice);
-
 console.log("Human choice: " + humanChoice);
 console.log("Computer choice: " + computerChoice);
 console.log("Round Result: " + playRound(humanChoice, computerChoice));
+console.log(`Human score: ${humanScore}`);
+console.log(`Computer score: ${computerScore}`);
