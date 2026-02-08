678e2226c656666c7adac6e2a5bcef5ea3e7fd12
Kenenn Lara <kenennlara@gmail.com>
2026-02-07 19:47:41 -0600

    Add function for playing 5 rounds and fix some issues
    Move each generated choice into playRound( ) for being able to generate each option more than once and simulate multiple rounds correctly.

--- index.js
+++ index.js
@@ -21,39 +21,71 @@ function getHumanChoice() {
   return userEntry.toLowerCase();
 }
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
+function playRound() {
+  const humanChoice = getHumanChoice();
+  const computerChoice = getComputerChoice();
+
+  console.log("Human choice: " + humanChoice);
+  console.log("Computer choice: " + computerChoice);
+
   if (humanChoice === computerChoice) {
     humanScore++;
     computerScore++;
-    return "Its a tie!";
+    console.log("Its a tie!");
   } else if (humanChoice === "rock" && computerChoice === "scissors") {
     humanScore++;
-    return "Rock beats scissors! Human wins!";
+    console.log("Rock beats scissors! Human wins!");
   } else if (humanChoice === "scissors" && computerChoice === "rock") {
     computerScore++;
-    return "Rock beats scissors! Computer wins!";
+    console.log("Rock beats scissors! Computer wins!");
   } else if (humanChoice === "paper" && computerChoice === "rock") {
     humanScore++;
-    return "Paper beats rock! Human wins!";
+    console.log("Paper beats rock! Human wins!");
   } else if (humanChoice === "rock" && computerChoice === "paper") {
     computerScore++;
-    return "Paper beats rock!  Computer wins!";
+    console.log("Paper beats rock!  Computer wins!");
   } else if (humanChoice === "scissors" && computerChoice === "paper") {
     humanScore++;
-    return "Scissors beats paper!  Human wins!";
+    console.log("Scissors beats paper!  Human wins!");
   } else if (humanChoice === "paper" && computerChoice === "scissors") {
     computerScore++;
-    return "Scissors beats paper!  Computer wins!";
+    console.log("Scissors beats paper!  Computer wins!");
   }
 }
 
-console.log("Human choice: " + humanChoice);
-console.log("Computer choice: " + computerChoice);
-console.log("Round Result: " + playRound(humanChoice, computerChoice));
-console.log(`Human score: ${humanScore}`);
-console.log(`Computer score: ${computerScore}`);
+function scores() {
+  console.log(`Human score: ${humanScore}`);
+  console.log(`Computer score: ${computerScore}`);
+}
+
+function playGame() {
+  console.log("ROUND 1");
+  playRound();
+  scores();
+  console.log(" ");
+
+  console.log("ROUND 2");
+  playRound();
+  scores();
+  console.log(" ");
+
+  console.log("ROUND 3");
+  playRound();
+  scores();
+  console.log(" ");
+
+  console.log("ROUND 4");
+  playRound();
+  scores();
+  console.log(" ");
+
+  console.log("ROUND 5");
+  playRound();
+  console.log(`Final Score Human: ${humanScore}`);
+  console.log(`Final Score Computer: ${computerScore}`);
+}
+
+playGame();
